4750d7618f57606259b7bbd5c6e485ba48c669ed
Noah Olatoye <noaholatoye@gmail.com>
2025-12-03 15:40:10 +0100

    Optimised paystack.ts handleDBAction

--- src/components/tests/calculate_coupon.ts
+++ src/components/tests/calculate_coupon.ts
@@ -1,3 +1,45 @@
+/**
+ * Calculates amount after percentage deduction.
+ * @param amount Base amount
+ * @param coupon CouponType object
+ * @returns Object with final amount or error details
+ */
+export const calculateCouponDeduction = (
+  amount: number,
+  currency: string,
+  coupon: any
+): { amount: number; detail?: string; discounted: boolean } => {
+  let deduction = 0;
+  if (coupon.discount_type === "percentage" || currency !== coupon.currency) {
+    if (amount < 0 || coupon.discount < 0 || coupon.discount > 100) {
+      return {
+        discounted: false,
+        amount: 0,
+        detail:
+          "Amount and percentage should be positive, and percentage should be between 0 and 100.",
+      };
+    }
+    deduction = (amount * coupon.discount) / 100;
+  } else if (coupon.discount_type === "amount") {
+    if (amount < 0 || coupon.amount < 0 || coupon.amount > amount) {
+      return {
+        discounted: false,
+        amount: coupon.amount,
+        detail:
+          coupon.amount > amount
+            ? "Coupon amount cannot be greater than actual product price."
+            : "Amount and discount should be positive.",
+      };
+    }
+    deduction = coupon.amount;
+  }
+  return {
+    amount: amount - deduction,
+    discounted: true,
+    detail: "Coupon applied successfully.",
+  };
+};
+
 const couponObj = {
   id: 1,
   total_claimed_coupon: 1,
@@ -17,15 +59,19 @@ const couponObj = {
   valid_from: "2025-01-16T03:04:00+01:00",
   valid_to: "2026-01-17T03:04:00+01:00",
   days_count: 7,
-  discount_type: "percentage",
+  discount_type: "amount",
   discount: 100,
-  amount: null,
+  amount: 1000,
   currency: "NGN",
   active: true,
   timestamp: "2025-01-16T03:06:58.401091+01:00",
   channel: "10ce8a17-2651-4ddc-86a9-bfe7926869fc",
   content_type: 7,
   students: [],
-};
+} as any;
 
+const cal = calculateCouponDeduction(10000, "NGN", couponObj);
 
+console.log("Expected amount: 9000 =========\n\n");
+console.log(cal);
+console.log("=========\n\n");
